68b2d5aaf499d41ce7e5ea8853c94c29bb761bb1
Vladimir Popov <vladimir@dokwork.ru>
2019-09-24 18:23:47 +0300

    add breakOn for persisted saga

--- src/main/scala/ru/dokwork/fasti/persistence/PersistedSaga.scala
+++ src/main/scala/ru/dokwork/fasti/persistence/PersistedSaga.scala
@@ -6,31 +6,34 @@ import ru.dokwork.fasti.Saga
 import shapeless.ops.hlist.Prepend
 import shapeless.{ ::, HList, HNil }
 
-final class PersistedSaga[F[_], A, B, S <: HList, Id, Encoded] private (
+final class PersistedSaga[F[_], A, B, S <: HList, TxId, Encoded] private (
     private val inner: Saga[F, A, B, S]
 )(implicit F: MonadError[F, Throwable]) {
 
-  def andThen[S2 <: HList, D](other: PersistedSaga[F, B, D, S2, Id, Encoded])(
+  def andThen[S2 <: HList, D](other: PersistedSaga[F, B, D, S2, TxId, Encoded])(
       implicit ev: Prepend[S, B :: S2]
-  ): PersistedSaga[F, A, D, ev.Out, Id, Encoded] =
-    new PersistedSaga[F, A, D, ev.Out, Id, Encoded](inner andThen other.inner)
+  ): PersistedSaga[F, A, D, ev.Out, TxId, Encoded] =
+    new PersistedSaga[F, A, D, ev.Out, TxId, Encoded](inner andThen other.inner)
 
   def completeOn[S2 <: HList, D](other: Saga[F, B, D, S2])(
       implicit ev: Prepend[S, B :: S2]
-  ): CompletedPersistedSaga[F, A, D, ev.Out, Id, Encoded] =
-    new CompletedPersistedSaga[F, A, D, ev.Out, Id, Encoded](inner andThen other)
+  ): CompletedPersistedSaga[F, A, D, ev.Out, TxId, Encoded] =
+    new CompletedPersistedSaga[F, A, D, ev.Out, TxId, Encoded](inner andThen other)
+
+  def breakOn(isFatal: PartialFunction[Throwable, Unit]): PersistedSaga[F, A, B, S, TxId, Encoded] =
+    new PersistedSaga(inner.breakOn(isFatal))
 }
 
-final class CompletedPersistedSaga[F[_], A, B, S <: HList, Id, Encoded] private[persistence] (
+final class CompletedPersistedSaga[F[_], A, B, S <: HList, TxId, Encoded] private[persistence] (
     private val inner: Saga[F, A, B, S]
 )(implicit F: MonadError[F, Throwable]) {
 
-  def apply(id: Id, a: A)(implicit persistence: SagaPersistence[F, Encoded, Id]): F[Either[Throwable, B]] =
+  def apply(id: TxId, a: A)(implicit persistence: SagaPersistence[F, Encoded, TxId]): F[Either[Throwable, B]] =
     inner(a).flatTap(_ => persistence.completed(id))
 
-  def restore(id: Id)(
+  def restore(id: TxId)(
       implicit decoder: DecodeStates[S, Encoded],
-      persistence: SagaPersistence[F, Encoded, Id]
+      persistence: SagaPersistence[F, Encoded, TxId]
   ): F[Either[Throwable, B]] =
     persistence
       .load(id)
@@ -43,6 +46,9 @@ final class CompletedPersistedSaga[F[_], A, B, S <: HList, Id, Encoded] private[
             )
           }
       }
+
+  def breakOn(isFatal: PartialFunction[Throwable, Unit]): CompletedPersistedSaga[F, A, B, S, TxId, Encoded] =
+    new CompletedPersistedSaga(inner.breakOn(isFatal))
 }
 
 object PersistedSaga {
@@ -50,14 +56,14 @@ object PersistedSaga {
   def apply[Id, Encoded]: Applier[Id, Encoded] = new Applier[Id, Encoded]
 
   final class Applier[Id, Encoded] {
-    def apply[F[_], A, B](action: A => F[B], compensate: (B, Throwable) => F[Unit])(getId: B => Id)(
+    def apply[F[_], A, B](action: A => F[B], compensate: (B, Throwable) => F[Unit])(getTxId: B => Id)(
         implicit F: MonadError[F, Throwable],
         encoder: Encoder[B, Encoded],
         persistence: SagaPersistence[F, Encoded, Id],
     ): PersistedSaga[F, A, B, HNil, Id, Encoded] = new PersistedSaga(
       Saga(
-        a => action(a).flatTap(b => persistence.persist(getId(b), encoder.encode(b))),
-        (b, cause) => compensate(b, cause) >> persistence.lastCompensated(getId(b), cause)
+        a => action(a).flatTap(b => persistence.persist(getTxId(b), encoder.encode(b))),
+        (b, cause) => compensate(b, cause) >> persistence.lastCompensated(getTxId(b), cause)
       )
     )
   }

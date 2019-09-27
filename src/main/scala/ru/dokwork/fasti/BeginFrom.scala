810e7fe21ab50f6f6812f593eb85fc2a04a66301
Vladimir Popov <vladimir@dokwork.ru>
2019-09-28 00:47:50 +0300

    rename implicit methods

--- src/main/scala/ru/dokwork/fasti/BeginFrom.scala
+++ src/main/scala/ru/dokwork/fasti/BeginFrom.scala
@@ -10,9 +10,9 @@ trait BeginFrom[S <: HList, P <: HList]
 object BeginFrom {
   def apply[S <: HList, P <: HList](implicit instance: BeginFrom[S, P]): BeginFrom[S, P] = instance
 
-  implicit def single[H, T <: HList]: BeginFrom[H :: T, H :: HNil] = new BeginFrom[H :: T, H :: HNil] {}
+  implicit def ev4single[H, T <: HList]: BeginFrom[H :: T, H :: HNil] = new BeginFrom[H :: T, H :: HNil] {}
 
-  implicit def list[H, TS <: HList, TP <: HList](implicit t: BeginFrom[TS, TP]): BeginFrom[H :: TS, H :: TP] =
+  implicit def ev4list[H, TS <: HList, TP <: HList](implicit t: BeginFrom[TS, TP]): BeginFrom[H :: TS, H :: TP] =
     new BeginFrom[H :: TS, H :: TP] { require(t ne null) }
 }
 
--- src/main/scala/ru/dokwork/fasti/persistence/DecodeStates.scala
+++ src/main/scala/ru/dokwork/fasti/persistence/DecodeStates.scala
@@ -16,19 +16,19 @@ object DecodeStates {
 
   type Aux[S <: HList, Encoded, P0 <: HList] = DecodeStates[S, Encoded] {type P = P0}
 
-  implicit def single[H, Encoded](
+  implicit def build4single[H, Encoded](
     implicit
     decoder: Decoder[H, Encoded]
   ): DecodeStates[H :: HNil, Encoded] = new DecodeStates[H :: HNil, Encoded] {
     type P = H :: HNil
 
     override def decode(stages: NonEmptyList[Encoded]): Try[(P, BeginFrom[H :: HNil, P])] = stages match {
-      case NonEmptyList(x, Nil) => decoder.decode(x).map(h => (h :: HNil, BeginFrom.single[H, HNil]))
+      case NonEmptyList(x, Nil) => decoder.decode(x).map(h => (h :: HNil, BeginFrom.ev4single[H, HNil]))
       case NonEmptyList(_, t) => Failure(new IllegalArgumentException(s"Unexpected elements $t."))
     }
   }
 
-  implicit def list[H, TS <: HList, Encoded](
+  implicit def build4list[H, TS <: HList, Encoded](
     implicit
     headDecoder: Decoder[H, Encoded],
     tailDecoder: DecodeStates[TS, Encoded]
@@ -38,12 +38,12 @@ object DecodeStates {
     override def decode(stages: NonEmptyList[Encoded]): Try[(P, BeginFrom[H :: TS, P])] =
       stages match {
         case NonEmptyList(_, Nil) =>
-          single[H, Encoded].decode(stages).asInstanceOf[Try[(P, BeginFrom[H :: TS, P])]]
+          build4single[H, Encoded].decode(stages).asInstanceOf[Try[(P, BeginFrom[H :: TS, P])]]
         case NonEmptyList(x, t) =>
           for {
             head <- headDecoder.decode(x)
             (tail, bf) <- tailDecoder.decode(NonEmptyList.fromListUnsafe(t))
-          } yield (head :: tail, BeginFrom.list[H, TS, tailDecoder.P](bf))
+          } yield (head :: tail, BeginFrom.ev4list[H, TS, tailDecoder.P](bf))
       }
   }
 }
